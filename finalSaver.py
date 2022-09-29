f9e85ced34e9bef430fbf4771318973735f46690
Mangachh <mangachh@gmail.com>
2022-09-29 09:37:36 +0200

    updated structure and readme

--- finalSaver.py
+++ finalSaver.py
@@ -1,23 +1,41 @@
 #!/usr/bin/python3
-'''
-A simple script to create a "not disturb" mode. Instead of disabling the screen saver
-it uses xset s settings. 
+""" 
+A simple script to enable/disable blanking mode & expose on xfce4 desktops, 
+changes the icon on the panel launcher.
+The script prevents the screensave to start.scre
+"""
 
-'''
 
-
+from dataclasses import dataclass
 from genericpath import isdir
 import subprocess
 import os
 from subprocess import CompletedProcess
 
-# TODO: all methods are static methods, maybe
-class Saver:
-    '''
-    Class for enable or disable the screensaver. 
-    It uses xset s options to assure compatibility.
-    '''
+@dataclass
+class PathInfo:
+    """Dataclass for all the path related info used in IconChanger.
+    It's easy an neat to have all in the same class, this way we can
+    reuse everything
+    """
     
+    XFCE_PATH = "/home/cobos/.config/xfce4/panel/"
+    DEF_FOLDER_NAME = "launcher"
+    FILE_NAME = "screen.desktop"
+    
+    # TODO: create fallbacks icons
+    ICON_ON = "/home/cobos/code/python/Saver_plugin/on.svg"
+    ICON_OFF = "/home/cobos/code/python/Saver_plugin/off.svg"
+    
+
+
+class Saver:    
+    """Class responsible for enabling/disabling the blanking using
+    subporcess.run
+    All the methods are static
+    """
+    
+    # TODO: maybe all this data in it's own class?
     XSET_OFF = "xset s off".split(" ")
     XSET_NO_BLANK = "xset s noblank".split(" ")
     XSET_NO_EXP = "xset s noexpose".split(" ")
@@ -31,9 +49,14 @@ class Saver:
     BLANKING_ON = "prefer blanking:  yes"        
         
         
-    @property
-    def is_enabled(self) -> bool:
-        '''Is the screensaver enabled? Checks using \"xset q\"'''
+    @staticmethod
+    def is_enabled() -> bool:
+        """
+        Checks if the blanking is activated with [xset q]
+
+        Returns:
+            bool: Is the blanking activated?
+        """
         res = subprocess.check_output(Saver.CHECK_STATUS_CMD, shell=True)
         
         print(f"Holaaaa {str(res)}")        
@@ -43,31 +66,48 @@ class Saver:
         else:
             return False      
             
+    @staticmethod
+    def disable_blanking() -> None:
+        """Disables he blanking on screen"""
         
-    def disable_screen_saver(self) -> None:
-        ''' Disables screensaver '''
         (xset, blank, expose) = Saver.__change_saver_opt(Saver.XSET_OFF, Saver.XSET_NO_BLANK, Saver.XSET_NO_EXP)    
                
         print(f"Disabled Saver:\n{xset}\n{blank}\n{expose}")  
         
-
-    def enable_screen_saver(self) -> None:
-        ''' Enables screensaver '''
+        
+    @staticmethod
+    def enable_blanking() -> None:
+        """Enables the blanking on screen"""
+        
         (xset, blank, expose) = Saver.__change_saver_opt(Saver.XSET_ON, Saver.XSET_BLANK, Saver.XSET_EXPOSE) 
           
         print(f"Enabled Saver:\n{xset}\n{blank}\n{expose}")   
         
      
-    def opposite_saver(self) -> None:
-        '''If the screensaver is enabled, disables it and viceversa'''
-        if self.is_enabled:
-            self.disable_screen_saver()
+    @staticmethod
+    def opposite_saver() -> None:
+        """
+        If the blanking is enabled, disables it and viceversa, if the blanking is disabled, enables it.
+        The method checks properly if is enabled, so it doesn't need an argument.
+        """
+        if Saver.is_enabled():
+            Saver.disable_blanking()
         else:
-            self.enable_screen_saver()
+            Saver.enable_blanking()
         
     @staticmethod
     def __change_saver_opt(xset: str, blank: str, expose: str) -> tuple[CompletedProcess, CompletedProcess, CompletedProcess]:
-        '''Changes the xset s options'''
+        """
+        Changes the blanking options.
+
+        Args:
+            xset (str): xset s on/off CMD order 
+            blank (str): xset s no/blanking CMD order
+            expose (str): sxet s expose CMD order 
+
+        Returns:
+            tuple[CompletedProcess, CompletedProcess, CompletedProcess]: completed processes
+        """
         xset_res = subprocess.run(xset)
         blank_res = subprocess.run(blank)
         expose_res = subprocess.run(expose)
@@ -77,30 +117,37 @@ class Saver:
  
    
 # TODO: installation of the launcher and che
-class ChangerIcon:
-    '''A class that changes the icon for the launcher at panel.
-    Hardcoded name at the moment'''
-    # hardcoded, later we'll search
+class IconChanger:
+    """A class that changes the icon of a launcher in a panel
+    All methods are static.
+    """
     
-    __XFCE_PATH = "/home/cobos/.config/xfce4/panel/"
-    
-    __DEF_FOLDER_NAME = "launcher"
-    __FILE = "screen.desktop"
-    
-    # TODO: create fallbacks icons
-    __ICON_ON = "/home/cobos/code/python/Saver_plugin/on.svg"
-    __ICON_OFF = "/home/cobos/code/python/Saver_plugin/off.svg"
     __ICON_TXT = "Icon"
     
     @staticmethod
-    def change_icon(state: bool) -> None:
-        '''Method that changes the icon based on the state'''
-        folder = ChangerIcon.search_path()
+    def change_icon(state: bool, parent_path: str, def_name: str, file_name: str, icon_on: str, icon_off:str) -> None:
+        """
+        Method that changes the icon based on the passed state. 
+
+        Args:
+            state (bool): the state of the app
+            parent_path (str): start searching path
+            def_name (str): default name or part of the name of the folder that contains the file. 
+                            As some folders are [launcher-2], [launcher-6], [launcher-n], uses a [contains]
+                            to check if the folder is candidate to have the file. in this example we'll pass
+                            ["launcher"] and it will check every folder which name contains ["launcher"]
+                    
+            file_name (str): the name of the file that we are searching
+            icon_on (str): path of the icon when the state is True
+            icon_off (str): path of the icon when the state is False
+        """
+        
+        folder = IconChanger.search_path(parent_path, def_name, file_name)
         
         if folder is None or "":
             return
         
-        path = f"{folder}/{ChangerIcon.__FILE}"
+        path = f"{folder}/{file_name}"
         print(path)
         
         # read lines
@@ -111,43 +158,63 @@ class ChangerIcon:
         with open(path, "r+") as f:
             
             for line in lines:
-                if ChangerIcon.__ICON_TXT in line:
+                if IconChanger.__ICON_TXT in line:
                     if state is True:
-                        line = f"{ChangerIcon.__ICON_TXT}={ChangerIcon.__ICON_ON}\n"
+                        line = f"{IconChanger.__ICON_TXT}={icon_on}\n"
                     else:
-                        line = f"{ChangerIcon.__ICON_TXT}={ChangerIcon.__ICON_OFF}\n"
-                    #
-                #
-                
+                        line = f"{IconChanger.__ICON_TXT}={icon_off}\n"
+                    
+                                
                 f.write(line)
                 
     @staticmethod
-    def search_path() -> str:
-        '''Searchs for the correct path of the launcher. Returns empty if not exists'''
+    def search_path(parent_path: str, def_name: str, file_name: str) -> str:
+        """
+        Returns the correct path of the launcher. First it checks on parent folder and then
+        looks for any folder that contains the def_name and then looks for the file. Then
+        returns the full path.
+        
+        If the file doesn't exists it return [""]
+
+        Args:
+            parent_path (str): start searching path
+            def_name (str): default name or part of the name of the folder that contains the file. 
+                            As some folders are [launcher-2], [launcher-6], [launcher-n], uses a [contains]
+                            to check if the folder is candidate to have the file. in this example we'll pass
+                            ["launcher"] and it will check every folder which name contains ["launcher"]
+                    
+            file_name (str): the name of the file that we are searching
+
+        Returns:
+            str: full path of the file if exists, [""] if not
+        """
+        
         print("Searching path...")
-        for fn in os.listdir(ChangerIcon.__XFCE_PATH):           
-            if ChangerIcon.__DEF_FOLDER_NAME in fn:                
-                dir_file = os.path.join(ChangerIcon.__XFCE_PATH,fn)                
+        for fn in os.listdir(parent_path):           
+            if def_name in fn:                
+                dir_file = os.path.join(parent_path,fn)                
                 print(f"Searching in folder: {dir_file}")
                 if os.path.isdir(dir_file):
                     for file in os.listdir(dir_file):
-                        if file == ChangerIcon.__FILE:
+                        if file == file_name:
                             print(f"Path found at: {dir_file}")
                             return dir_file
-                        #
-                    #
-                #
-            #
-        #
-        
+                             
         return ""
  
     
 # program run
-if __name__ == "__main__": 
-    s = Saver()
-    s.opposite_saver()
-    ChangerIcon.change_icon(s.is_enabled)
+if __name__ == "__main__":     
+    # change the saver
+    Saver.opposite_saver()
+    
+    #change the icon
+    IconChanger.change_icon(Saver.is_enabled(), 
+                            PathInfo.XFCE_PATH, 
+                            PathInfo.DEF_FOLDER_NAME, 
+                            PathInfo.FILE_NAME, 
+                            PathInfo.ICON_ON, 
+                            PathInfo.ICON_OFF)
     
     input("Press any key")
     
--- readme.md
+++ readme.md
@@ -1,7 +1,27 @@
 
-## Installation
- TODO: make an installer to put files on the folder. Rigth now the paths are hardcoded
+# BlankOnOff
 
+A simple plugin for the xfce4 panel that enables-disables the blanking of the screen. This way the
+Screen Saver doesn't start.
+
+## How it works
+
+All the operations are done by the *finalSaver.py* script (to be renamed). 
+First the script checks if the blanking is on or off with a subprocess and
+*xset q* as a command. Then, if the blanking is on, it turns it off or viceversa
+using *xset s on/off* via subprocesses.
+
+Then it changes the icon of the launcher. Right now the launcher has to be put on by hand
+and change the name accordingly.
+
+
+## Installation
+ TODO: make an installer to put files on the folder. Right now the paths are hardcoded
+
+## Known issues:
+
+*Worth checking*: it seems that the changes doesn't last when the session is over. So, if by default the blinking is on 
+in every new run the blinking is going to be on regardless the app.
 
 
 ## Liscense:
