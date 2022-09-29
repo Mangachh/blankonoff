293b8097e3c58dca156f36e55f964719dac8963a
Mangachh <mangachh@gmail.com>
2022-09-29 13:01:34 +0200

    register panel, copy launcher, working

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from curses import panel
+from curses.ascii import isdigit
 import subprocess
 import os
 import shutil
@@ -8,13 +10,19 @@ import shutil
 # use copy, not mv.
 
 PATH = "/.local/share/blankonoff/"
+HOME = os.path.expanduser("~")
+APP_NAME = "screen.desktop"
 
 
-def create_directory() -> bool:    
-    home = os.path.expanduser('~')
-    source = f"{os.getcwd()}/"
-    destination = f"{home}{PATH}"
+def get_installation_paths() -> tuple[str, str]:
+    source = os.path.dirname(os.path.abspath(__file__))
+    destination = f"{HOME}{PATH}"
     
+    return (source, destination)
+    
+    
+def create_installation_directory(source: str, destination: str) -> bool:   
+    # cambiar eso para file     
     print(f"Desired Path: {destination}")
     print(f"Current Path: {source}")
     
@@ -37,11 +45,10 @@ def create_directory() -> bool:
             print("Directory already exists.")
         
         #move files
-        return move_files(source, destination)
+        #return move_files(source, destination)
         
     
-    return True
-                
+    return True           
     
 
 
@@ -59,6 +66,9 @@ def move_files(source: str, destination: str) -> bool:
             except FileNotFoundError:
                 print("File not found")
                 return False
+            except shutil.SameFileError:
+                print("Origin and destination are the same")
+                return True # true 'cause the files are but don't know if the app is
             except:
                 print("Error")
                 return False
@@ -66,14 +76,81 @@ def move_files(source: str, destination: str) -> bool:
     
     return True
 
+CMD_ADD_LAUNCHER = "xfce4-panel --add=launcher"
+
+CMD_GET_PANELS = "xfconf-query -c xfce4-panel -p /panels"
+CMD_GET_IDS = "xfconf-query -c xfce4-panel -p /panels/panel-%s/plugin-ids"
+CMD_GET_TYPE = "xfconf-query -c xfce4-panel -p /plugins/plugin-%s"
+CMD_GET_ITEMS = "xfconf-query -c xfce4-panel -p /plugins/plugin-%s/items "
 
 
-if __name__ == "__main__":
-    created = create_directory()
+def register_to_panel() -> str:
     
+    print("Adding launcher to panel")
+    resp = subprocess.run(CMD_ADD_LAUNCHER, shell=True)   
+    
+    # get all the plugins id
+    raw_ids= subprocess.check_output(CMD_GET_PANELS, shell=True).decode("UTF-8")
+    print(raw_ids)
+    panel_ids = [line for line in raw_ids.splitlines() if line.isdigit()]            
+    
+    print(panel_ids)
+    final_id = ""
+    
+    # for each panel, look all the plugins that are launcher
+    for panel in panel_ids:
+        num = str(panel);
+        
+        raw_ids = subprocess.check_output(CMD_GET_IDS % num, shell=True).decode("UTF-8")
+        plugins = [i for i in raw_ids.splitlines() if i.isdigit()]
+        
+        for id in plugins:
+            resp = subprocess.check_output(CMD_GET_TYPE % str(id), shell=True)
+            
+            print(resp)
+            
+            if b"launcher" in resp:
+                print(f"Found launcher at {id}")
+                # llamamos a ver si tiene items
+                resp_it = subprocess.run(CMD_GET_ITEMS % str(id), shell=True)
+                
+                if resp_it.returncode == 1:
+                    print("Found id for new launcher")
+                    return id
+                
+    return ""
+
+def set_launcher(id: str, source: str, destination:str, filename:str) -> bool:
+    
+    try:
+        os.makedirs(destination)
+        shutil.copy(f"{source}/{filename}", f"{destination}/{filename}")
+    except Exception as e:
+        print(e)
+        return False
+
+    return True
+    
+    
+def main():
+    (source, destination) = get_installation_paths()
+    created = create_installation_directory(source, destination)  
+      
     if created:
-        # register the launcher
-        pass
+        if move_files(source, destination) == False:
+            return          
+            
+        panel_id = register_to_panel()
+        
+        if panel_id == "":
+            return
+        
+        destination = HOME + "/.config/xfce4/panel/launcher-%s" % panel_id  
+        set_launcher(panel_id, source, destination, APP_NAME)
+         
+    
+if __name__ == "__main__":
+    main()
     
         
         
